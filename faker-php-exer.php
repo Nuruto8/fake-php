d7f9a01c445747666b52d84ee1e13ddd8127e6ad
Bry <etoquillab@gmail.com>
2025-02-28 14:04:39 +0800

    creating a data base connection and a officeIds

---
+++ faker-php-exer.php
@@ -0,0 +1,19 @@
+<?php 
+require 'vendor/autoload.php'; 
+
+$faker = Faker\Factory::create('en_PH');
+$pdo = new PDO("mysql:host=localhost;dbname=faker", "root", "202280287PSU");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$officeIds = [];
+for ($i = 0; $i < 50; $i++) {
+    $stmt = $pdo->prepare("INSERT INTO Office (name, contactnum, email, address, city, country, postal) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->execute([
+        $faker->company, $faker->phoneNumber, $faker->email, $faker->address, $faker->city, "Philippines", $faker->postcode
+    ]);
+    $officeIds[] = $pdo->lastInsertId();
+}
+
+
+echo "Fake data inserted successfully!";
+?> 
